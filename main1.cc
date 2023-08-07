a99bae0421565e56d1836bc65ea2b2c70d9e4c72
Ratib-Qahhar <ratibarif@yahoo.com>
2023-08-07 18:05:47 -0400

    asking for user info

--- main1.cc
+++ main1.cc
@@ -1,7 +1,31 @@
 #include <iostream>
-using namespace std;
-int main(){
-    cout <<"Hello world"<< endl;
+#include <string>
+
+class Person {
+private:
+    std::string name;
+    int age;
+
+public:
+    Person(const std::string& n, int a) : name(n), age(a) {}
+
+    void introduce() {
+        std::cout << "Hello, my name is " << name << " and I am " << age << " years old." << std::endl;
+    }
+};
+
+int main() {
+    std::string name;
+    int age;
+
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    Person person(name, age);
+    person.introduce();
+
+    return 0;
 }
-// new line added here this is the change made
-// this is main1
