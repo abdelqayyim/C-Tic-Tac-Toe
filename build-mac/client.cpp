9663b8087f0924d9edf4cfcc3864bbdc52028a4f
abdelqayyim <abdelymaster@gmail.com>
2023-09-03 15:20:35 +0100

    The Client.cpp file now works with server.py, now I have to implement the client into the game

--- build-mac/client.cpp
+++ build-mac/client.cpp
@@ -4,24 +4,27 @@
 #include <boost/asio.hpp>
 using namespace std;
 
-void receive_messages(boost::asio::ip::tcp::socket& socket) {
-    try {
-        boost::asio::streambuf buffer;
-        while (true) {
-            boost::asio::read_until(socket, buffer, '\n');
-            istream input(&buffer);
-            string msg_received;
-            getline(input, msg_received);
-            
-            cout << "Server: " << msg_received << endl;
-            if (msg_received == "exit") {
-                break;
+class Client {
+public:
+    void receive_messages(boost::asio::ip::tcp::socket& socket) {
+        try {
+            boost::asio::streambuf buffer;
+            while (true) {
+                boost::asio::read_until(socket, buffer, '\n');
+                istream input(&buffer);
+                string msg_received;
+                getline(input, msg_received);
+                
+                cout << "Server: " << msg_received << endl;
+                if (msg_received == "exit") {
+                    break;
+                }
             }
+        } catch (const exception& e) {
+            cerr << "Error: " << e.what() << endl;
         }
-    } catch (const exception& e) {
-        cerr << "Error: " << e.what() << endl;
     }
-}
+};
 
 int main() {
     boost::asio::io_context ioContext;
@@ -32,7 +35,8 @@ int main() {
 
     cout << "New client created:" << endl;
 
-    thread receive_thread(receive_messages, ref(socket));
+    Client client; // Create an instance of the Client class
+    std::thread receive_thread([&client, &socket]() { client.receive_messages(socket); }); // Using lambda
     receive_thread.detach();
 
     while (true) {
